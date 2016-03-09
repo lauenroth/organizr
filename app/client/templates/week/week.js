ad865c0c673dadbe877a21f21b6df1692171f0fe
Jörg Lauenroth <jorg.lauenroth@inthepocket.mobi>
2016-03-09 12:42:03 +0100

    Show items of the next 7 days

--- app/client/templates/week/week.js
+++ app/client/templates/week/week.js
@@ -4,10 +4,28 @@
 Template.Week.events({
 });
 
+Template.Week.Items = function() {
+  let week = {
+    start: moment().startOf('day').toDate(),
+    end: moment().add(7, 'days').endOf('day').toDate(),
+  };
+
+  return Items.find({date: {$gte: week.start, $lt: week.end}});
+}
+
 /*****************************************************************************/
 /* Week: Helpers */
 /*****************************************************************************/
 Template.Week.helpers({
+
+  hasItems: function() {
+    return Template.Week.Items().fetch().length;
+  },
+
+  items: function() {
+    return Template.Week.Items();
+  }
+
 });
 
 /*****************************************************************************/
