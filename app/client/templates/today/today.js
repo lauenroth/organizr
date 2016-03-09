1455ef2d9d546e4ea7278710626a397014b5bb7c
Jörg Lauenroth <jorg.lauenroth@inthepocket.mobi>
2016-03-09 12:41:37 +0100

    Show items of today

--- app/client/templates/today/today.js
+++ app/client/templates/today/today.js
@@ -4,10 +4,28 @@
 Template.Today.events({
 });
 
+Template.Today.Items = function() {
+  let today = {
+    start: moment().startOf('day').toDate(),
+    end: moment().endOf('day').toDate(),
+  };
+
+  return Items.find({date: {$gte: today.start, $lte: today.end}});
+}
+
 /*****************************************************************************/
 /* Today: Helpers */
 /*****************************************************************************/
 Template.Today.helpers({
+
+  hasItems: function() {
+    return Template.Today.Items().fetch().length;
+  },
+
+  items: function() {
+    return Template.Today.Items();
+  }
+
 });
 
 /*****************************************************************************/
