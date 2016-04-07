699a3acaf607cfac93d70126242478b59bb73f46
Jörg Lauenroth <jorg.lauenroth@inthepocket.mobi>
2016-04-07 08:41:12 +0200

    Option to show completed items

--- app/client/templates/list/list.js
+++ app/client/templates/list/list.js
@@ -4,6 +4,30 @@ Meteor.subscribe('lists');
 /* List: Event Handlers */
 /*****************************************************************************/
 Template.List.events({
+
+  'click .sub-menu-icon': function() {
+    $('body').addClass('show-sub-menu');
+  },
+
+  'click .sub-menu li': function() {
+    $('body').removeClass('show-sub-menu');
+  },
+
+  'click .sub-menu .delete': function() {
+    const list = this;
+    const itemsInList = Items.find({list: list._id}).fetch();
+    confirmDialog('Do you really wanna delete the list' + (itemsInList.length > 0 ? ' and all its items' : '') + '?', function() {
+      itemsInList.forEach(item => {
+        Items.remove({_id: item._id});
+      });
+      Lists.remove({_id: list._id});
+    });
+  },
+
+  'click .sub-menu .toggle-done': function() {
+    Session.set('showCompletedTasks', !!Session.get('showCompletedTasks'));
+  }
+
 });
 
 /*****************************************************************************/
@@ -14,6 +38,14 @@ Template.List.Items = function() {
   if (list) {
     const itemsWithDate = Items.find({list: list._id, owner: Meteor.userId(), done : {$exists: false}, date: {$exists: true} }, {sort: {date: 1} }).fetch();
     const itemsWithoutDate = Items.find({list: list._id, owner: Meteor.userId(), done : {$exists: false}, date: {$exists: false} }).fetch();
+    return itemsWithDate.concat(itemsWithoutDate);
+  }
+}
+Template.List.CompletedItems = function() {
+  const list = Session.get('currentList');
+  if (list) {
+    const itemsWithDate = Items.find({list: list._id, owner: Meteor.userId(), done : {$exists: true}, date: {$exists: true} }, {sort: {date: 1} }).fetch();
+    const itemsWithoutDate = Items.find({list: list._id, owner: Meteor.userId(), done : {$exists: true}, date: {$exists: false} }).fetch();
     return itemsWithDate.concat(itemsWithoutDate);
   }
 }
@@ -29,6 +61,38 @@ Template.List.helpers({
 
   items: function() {
     return Template.List.Items();
+  },
+
+  hasCompletedItems: function() {
+    const items = Template.List.CompletedItems();
+    if (items) {
+      return items.length > 0;
+    }
+    return false;
+  },
+
+  hasNoItemsAtAll: function() {
+    var items = Template.List.Items();
+    if (items && items.length > 0) {
+      return false;
+    }
+    items = Template.List.CompletedItems();
+    if (items && items.length > 0) {
+      return false;
+    }
+    return true;
+  },
+
+  completedItems: function() {
+    return Template.List.CompletedItems();
+  },
+
+  isSpecialList: function() {
+    return this._id === 'inbox';
+  },
+
+  showCompletedTasks: function() {
+    return !!Session.get('showCompletedTasks');
   },
 
 });
